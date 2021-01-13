b024e627061e9b360f1fe8ae7b0e0e2f543311d6
Javier Revilla <jrevilla.pcm@gmail.com>
2021-01-12 21:27:16 -0500

    Added models: Cell and Board. Modified settings.

--- games/minesweeper/admin.py
+++ games/minesweeper/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Cell, Board
 
 # Register your models here.
+class CellAdmin(admin.ModelAdmin):
+    list_display = ("name", "x", "y", "value")
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("name", "rows", "cols", "mines")
+
+admin.site.register(Cell, CellAdmin)
+admin.site.register(Board, BoardAdmin)
--- games/minesweeper/models.py
+++ games/minesweeper/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Cell(models.Model):
+    name = models.CharField(max_length=16)
+
+    x = models.IntegerField()
+    y = models.IntegerField()
+    value = models.IntegerField()   # 1 to 8
+
+    hidden = models.BooleanField()
+    mined = models.BooleanField()
+    flagged = models.BooleanField()
+
+
+    def __init__(self):
+        print('Cell - init')
+
+    def __str__(self):
+        return f"Cell: {self.x}, {self.y}, {self.value}"
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=16)
+    rows = models.IntegerField()
+    cols = models.IntegerField()
+    mines = models.IntegerField()
+
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    duration = models.DurationField()
+
+    nr_hidden_cells = models.IntegerField()
+    state = models.CharField(max_length=16)
+    
+    cells = models.ManyToManyField(Cell, blank=True)
+
+
+    def __init__(self):
+        print('Board - init')
+
+    def __str__(self):
+        return f"Board: {self.name}, {self.y}, {self.value}"
+
