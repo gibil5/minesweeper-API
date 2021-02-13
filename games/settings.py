54f46d8789f367af9b7a785a5220c5886380450a
Javier Revilla <jrevilla.pcm@gmail.com>
2021-02-13 14:14:12 -0500

    Settings.

--- games/settings.py
+++ games/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY="7e53cf673867e796dea84ef72d2b083abd4cae462656a5ee"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
