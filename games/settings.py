98172c80e7e2e6e9d21695c6b27a9739830cc88c
Javier Revilla <jrevilla.pcm@gmail.com>
2021-02-15 15:08:57 -0500

    Solved prod bugs.

--- games/settings.py
+++ games/settings.py
@@ -89,10 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'games_minesweeper',
-        'USER': 'postgres',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        #'USER': 'postgres',
+        #'PASSWORD': 'DB_PASSWORD',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
     }
 }
 
