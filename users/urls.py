75ccb1202b90b521f5904b744fb65157097ef7a9
Javier Revilla <jrevilla.pcm@gmail.com>
2021-06-26 16:37:04 -0500

    User routes corr.

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,8 @@ from . import views
 # Routing
 urlpatterns = [
     path("", views.index, name="index"),
-    path("users/index/", views.users, name="users"),
+    #path("users/index/", views.users, name="users"),
+    path("users/", views.users, name="users"),
     path('users/show/<int:user_id>/', views.show, name="users_show"),
     path("users/login/", views.login_view, name="login"),
     path("users/logout/", views.logout_view, name="logout"),
