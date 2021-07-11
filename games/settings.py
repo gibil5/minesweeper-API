15cf3e3b526b0cbfc8e1d1a47704e42bbb85303f
Javier Revilla <jrevilla.pcm@gmail.com>
2021-07-10 22:55:48 -0500

    Cleaned testing.

--- games/settings.py
+++ games/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-#DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 
 ALLOWED_HOSTS = []
@@ -139,7 +139,7 @@ DATETIME_FORMAT = "d/m/y - P"
 USE_L10N = False
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
